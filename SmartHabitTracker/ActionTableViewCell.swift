cd5b3ae49477f64e145dfb34978213b163b35619
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-09-16 14:15:51 +0200

    MainStoryboard - Custom ActionTableViewCell created

---
+++ SmartHabitTracker/ActionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActionTableViewCell.swift
+//  SmartHabitTracker
+//
+//  Created by Daniel Lohse on 9/16/16.
+//  Copyright © 2016 Superstudio. All rights reserved.
+//
+
+import UIKit
+
+class ActionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
