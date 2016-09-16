12e8138905532ef99c93d8a351115c0fe7f46687
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-09-16 13:53:18 +0200

    TableViewController added to Storyboard

---
+++ SmartHabitTracker/Action.swift
@@ -0,0 +1,22 @@
+//
+//  Action.swift
+//  SmartHabitTracker
+//
+//  Created by Daniel Lohse on 9/16/16.
+//  Copyright © 2016 Superstudio. All rights reserved.
+//
+
+import Foundation
+
+
+class Action{
+    var timestamp : NSDate
+    var actionName : String
+    
+    
+    
+    init(actionName : String) {
+        self.actionName = actionName
+        self.timestamp = NSDate()
+    }
+}
