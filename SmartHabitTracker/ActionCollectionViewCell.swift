c56b24422c9174fae4fff4e5feebbf62a47b1f69
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-09-17 17:53:36 +0200

    ChooseActionCollectionView - scene added including delegate. datasource

---
+++ SmartHabitTracker/ActionCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ACtionCollectionViewCell.swift
+//  SmartHabitTracker
+//
+//  Created by Daniel Lohse on 9/17/16.
+//  Copyright © 2016 Superstudio. All rights reserved.
+//
+
+import UIKit
+
+class ActionCollectionViewCell: UICollectionViewCell {
+    
+}
--- SmartHabitTracker/ChooseActionViewController.swift
+++ SmartHabitTracker/ChooseActionViewController.swift
@@ -9,11 +9,34 @@
 import Foundation
 import UIKit
 
-class ChooseActionViewController : UIViewController{
+class ChooseActionViewController : UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    let reuseIdentifier = "chooseActionCell"
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     
     override func viewDidLoad() {
     
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
     
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! ActionCollectionViewCell
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+
 }
