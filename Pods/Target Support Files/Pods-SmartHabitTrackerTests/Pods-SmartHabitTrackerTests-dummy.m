3b5f78a5514ac74797995857bf8a59501c422402
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2016-09-20 17:38:31 +0200

    Cocoapod init

---
+++ Pods/Target Support Files/Pods-SmartHabitTrackerTests/Pods-SmartHabitTrackerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartHabitTrackerTests : NSObject
+@end
+@implementation PodsDummy_Pods_SmartHabitTrackerTests
+@end
---
+++ Pods/Target Support Files/Pods-SmartHabitTrackerUITests/Pods-SmartHabitTrackerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartHabitTrackerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SmartHabitTrackerUITests
+@end
